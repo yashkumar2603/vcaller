deec9096cd34fd372c6ad83e34e5863e5fe4ff99
yashkumar2603 <yash.2603kumar@gmail.com>
2024-06-22 13:28:48 +0530

    added packages and setup room logic

--- src/Room.js
+++ src/Room.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
 const Room = () => {
-    return (
-        <div>
-            Room
-        </div>
-    )
-}
+    const { roomID } = useParams();
+    const meeting = async (element) => {
+        const appID = 97892940;
+        const serverSecret = "710003af9399973a9e8227b72ca26303";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            roomID,
+            Date.now().toString(),
+            "Yash"
+        );
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
 
-export default Room
+        zp.joinRoom({
+            container: element,
+            scenario: {
+                mode: ZegoUIKitPrebuilt.GroupCall,
+            },
+        });
+    };
+
+    return <div ref={meeting} style={{ width: "100vw", height: "100vh" }}></div>;
+};
+
+export default Room;
